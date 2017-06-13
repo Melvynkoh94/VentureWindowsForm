d4bc7899748ddff9858ea90f4ef30efad99f7bdc
Edmund Tay <edmundttj@crimsonlogic.com>
2017-06-13 10:24:18 +0800

    Initial commit

---
+++ IPOS.Dash.Admin.Service/WipoHague.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IPOS.Dash.Admin.Data;
+
+namespace IPOS.Dash.Admin.Service
+{
+    public class WipoHague
+    {
+        IposAdminEntities db = new IposAdminEntities();
+
+        public List<FCT_DS_WIPOHague> GetAll()
+        {
+            return db.FCT_DS_WIPOHague.Where(w => w.IsDeleted != true).ToList();
+        }
+
+    }
+}
---
+++ IPOS.Dash.Admin.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IPOS.Dash.Admin.Web.MvcApplication" Language="C#" %>
