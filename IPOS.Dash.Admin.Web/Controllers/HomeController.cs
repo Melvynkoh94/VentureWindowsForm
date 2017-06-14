740f68f8f8e41cde7106b9bca11da6b560e0a9e6
Edmund Tay <edmundttj@crimsonlogic.com>
2017-06-14 16:29:43 +0800

    Added setup.sql

--- IPOS.Dash.Admin.Web/Controllers/HomeController.cs
+++ IPOS.Dash.Admin.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IPOS.Dash.Admin.Data;
+using IPOS.Dash.Admin.Service;
+using IPOS.Dash.Admin.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +9,23 @@ using System.Web.Mvc;
 
 namespace IPOS.Dash.Admin.Web.Controllers
 {
-    public class HomeController : Controller
+    public class WipoHagueController : Controller
     {
-        public ActionResult Index()
+        public ActionResult List()
         {
-            return View();
+            WipoHagueService service = new WipoHagueService();
+            List<FCT_DS_WIPOHague> dataList = service.GetAll();
+
+            List<WipoHagueViewModel> wipoList = new List<WipoHagueViewModel>();
+            foreach(FCT_DS_WIPOHague currentData in dataList)
+            {
+                WipoHagueViewModel wipo = new WipoHagueViewModel();
+                wipo.Id = currentData.Id;                
+                wipo.InternationalRegistrations = currentData.IntlRegistrations;
+                wipoList.Add(wipo);
+            }
+
+            return View(wipoList);
         }
 
         public ActionResult About()
---
+++ IPOS.Dash.Admin.Web/Models/WipoHagueViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPOS.Dash.Admin.Web.Models
+{
+    public class WipoHagueViewModel
+    {
+        public Guid Id { get; set; }
+        public int InternationalRegistrations { get; set; }
+
+    }
+}
